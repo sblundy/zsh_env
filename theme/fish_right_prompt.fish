c7445839d461e7ceb263c0289afbee2132b84a02
sblundy <el_gonzzo@hotmail.com>
2018-03-04 13:02:47 -0800

    Right Prompt Improvements * Moved dirty icon to front * Changed git icon to a most monospace friendly glyph * Removed newlines from echo calls * Added branch status vs remote * Added parentheses to branch name

--- theme/fish_right_prompt.fish
+++ theme/fish_right_prompt.fish
@@ -1,23 +1,43 @@
 function fish_right_prompt -d 'Write out the right prompt'
     if vcs.present
+        if set -l dirty (vcs.dirty)
+            set_color --bold red
+            echo -n "± "
+        end
         if set -l vcs_name (vcs.name)
             set_color --bold green
             switch $vcs_name
                 case git
-                    echo '⎇ '
+                    # TODO find better symbol
+                    echo -n 'ɤ '
                 case svn
-                    echo 's '
+                    echo -n 's '
                 case hg
-                    echo '☿ '
+                    echo -n '☿ '
             end
         end
-        if set -l dirty (vcs.dirty)
-            set_color --bold red
-            echo "± "
+
+        set -l vcs_status (vcs.status)
+        switch "$vcs_status"
+            case ahead
+                set_color --bold yellow
+                echo -n '↑ '
+            case behind
+                set_color --bold yellow
+                echo -n '↓ '
+            case diverged
+                set_color --bold yellow
+                echo -n '⇅ '
+            case detached
+                set_color --bold red
+                echo -n '╳ '
+            case clean
+                set_color green
+                echo -n '✓ '
         end
         set -l branch_name (vcs.branch)
         set_color --bold yellow
-        echo "$branch_name"
+        echo -n "($branch_name)"
         set_color normal
     end
 end
