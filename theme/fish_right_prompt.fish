6185a83363692dae505995fe4cdc75d07c3c2d19
sblundy <el_gonzzo@hotmail.com>
2018-03-04 08:31:38 -0800

    Personal Theme * Right prompt will display the current branch and indicate whether local repro is dirty

--- theme/fish_right_prompt.fish
+++ theme/fish_right_prompt.fish
@@ -1,7 +1,23 @@
 function fish_right_prompt -d 'Write out the right prompt'
-	set -l git_prompt (__fish_git_prompt "%s")
-
-	if test -n "$git_prompt"
-		echo -n "$git_prompt"
-	end
+    if vcs.present
+        if set -l vcs_name (vcs.name)
+            set_color --bold green
+            switch $vcs_name
+                case git
+                    echo '⎇ '
+                case svn
+                    echo 's '
+                case hg
+                    echo '☿ '
+            end
+        end
+        if set -l dirty (vcs.dirty)
+            set_color --bold red
+            echo "± "
+        end
+        set -l branch_name (vcs.branch)
+        set_color --bold yellow
+        echo "$branch_name"
+        set_color normal
+    end
 end
