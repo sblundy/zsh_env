63b95f0e1ffc4779277c9576e7e22156ff88b0b5
sblundy <el_gonzzo@hotmail.com>
2018-03-04 07:56:07 -0800

    Personal Theme * Migrate to new structure

--- theme/fish_right_prompt.fish
+++ theme/fish_right_prompt.fish
@@ -1,6 +1,5 @@
 function fish_right_prompt -d 'Write out the right prompt'
-
-	set -l git_prompt (__fish_git_prompt "♈ %s")
+	set -l git_prompt (__fish_git_prompt "%s")
 
 	if test -n "$git_prompt"
 		echo -n "$git_prompt"
