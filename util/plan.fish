135ff10309761307d4392bdbee7a092ef7bea11c
sblundy <el_gonzzo@hotmail.com>
2018-04-11 17:46:34 -0700

    Bug Fix * Fixing handling of arguments after the action when there are none

--- util/plan.fish
+++ util/plan.fish
@@ -29,11 +29,14 @@ function plan --description='Daily planning util'
         end
         cp $filename "$filename.bk"
         command python3 "$PLANS_HOME/plan.py" $filename $action $argv[2..-1]
+        command python3 "$PLANS_HOME/plan.py" $filename current
     case '*'
         if not test -e "$filename"
             echo "Today's plan hasn't been initialized" 1>&2
             return 1
         end
-        command python3 "$PLANS_HOME/plan.py" $filename $action $argv[2..-1]
+        test 1 -lt (count $argv); and set -l CMD_ARGS $filename $action $argv[2..-1]; or set -l CMD_ARGS $filename $action
+        echo $CMD_ARGS
+        command python3 "$PLANS_HOME/plan.py" $CMD_ARGS
     end
 end
