a84c8c29907ebd148011daf5d81999d5c3c7aaf1
sblundy <el_gonzzo@hotmail.com>
2018-04-21 10:09:50 -0700

    Adding GoPath info to the right prompt

--- theme/fish_right_prompt.fish
+++ theme/fish_right_prompt.fish
@@ -1,3 +1,6 @@
+set -q _prompt_gopath_color
+                or set _prompt_gopath_color magenta
+
 function fish_right_prompt -d 'Write out the right prompt'
     if vcs.present
         if set -l dirty (vcs.dirty)
@@ -38,6 +41,36 @@ function fish_right_prompt -d 'Write out the right prompt'
         set -l branch_name (vcs.branch)
         set_color --bold yellow
         echo -n "($branch_name)"
+        set_color normal
+    end
+
+    if test -n "$GOPATH"
+        echo -n ' '
+        switch $PWD
+        case "$GOPATH"
+            set_color --bold $_prompt_gopath_color
+            echo -n  '.'
+        case "$GOPATH*"
+            set -l TMP_DIR "$PWD"
+            set -l GOPATH_MSG ""
+            while test $TMP_DIR != $GOPATH
+                set TMP_DIR (dirname $TMP_DIR)
+                set GOPATH_MSG "../$GOPATH_MSG"
+            end
+            set_color --bold $_prompt_gopath_color
+            echo -n "$GOPATH_MSG"
+        case "*"
+            set_color $_prompt_gopath_color
+            echo -n \uE0B2
+            set_color black
+            set_color --background $_prompt_gopath_color
+            set realhome ~
+            set -l tmp_gopath (string replace -r '^'"$realhome"'($|/)' '~$1' $GOPATH)
+            set -q gopath_dir_length
+                or set -l gopath_dir_length 1
+            string replace -ar '(\.?[^/]{'"$gopath_dir_length"'})[^/]*/' '$1/' "$tmp_gopath"
+        end
+
         set_color normal
     end
 end
